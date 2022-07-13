c97d228568d23aa148de2d001207b104999c748d
Lucas Castro <professorlucascastro@gmail.com>
2022-07-12 23:24:04 -0300

    Criando projeto de backend

---
+++ backend/src/main/java/com/lucastrodev/gerenciadorDeVendas/GerenciadorDeVendasApplication.java
@@ -0,0 +1,13 @@
+package com.lucastrodev.gerenciadorDeVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucastrodev/gerenciadorDeVendas/GerenciadorDeVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucastrodev.gerenciadorDeVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
